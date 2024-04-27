2778603b36d9bd34a163e8476ba98cfeffb50454
Anaïs Héloïse <anais.heloise@gmail.com>
2024-04-27 21:13:38 +0200

    updated packages to lts

--- components/layout.tsx
+++ components/layout.tsx
@@ -1,9 +1,14 @@
-import Head from 'next/head';
 import styles from '../styles/layout.module.scss';
 import Navbar from './navbar';
 import ThemeSwitch from './ThemeSwitch';
+import { Metadata } from 'next';
 
-export const siteTitle = 'Japan 80s'
+export const siteTitle = 'Japan 80s';
+
+export const metadata: Metadata = {
+    title: "City Pop'in",
+    description: ""
+}
 
 export default function Layout({
     children,
@@ -12,23 +17,6 @@ export default function Layout({
 }) {
     return (
         <div className={styles.container}>
-            <Head>
-                <title>City Pop'in</title>
-                <link rel="icon" href="/favicon.ico" />
-                <meta
-                    name="description"
-                    content="Information about life and music in Japan in the 80s"
-                />
-                <meta
-                    property="og:image"
-                    content={`https://og-image.vercel.app/${encodeURI(
-                        siteTitle
-                    )}.png?theme=light&md=0&fontSize=75px&images=https%3A%2F%2Fassets.zeit.co%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fnextjs-black-logo.svg`}
-                />
-                <meta name="og:title" content={siteTitle} />
-                <meta name="twitter:card" content="summary_large_image" />
-            </Head>
-
             <Navbar />
             <div className={styles.logo}>
                 <span>C</span><span>i</span><span>t</span><span>y</span>
